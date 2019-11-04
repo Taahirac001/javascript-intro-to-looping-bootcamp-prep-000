790991f01077b5e7a0b2763ce056c84bb7d604fe
Taahira Carter <taahirac001@gmail.com>
2019-11-04 04:17:59 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,30 @@
+function forLoop(array){
+  for(var i = 0; i < 26; i++){
+   if(i === 1){
+     array.concat(['I am 1 strange loop.']);
+   } else {
+     array.concat([`I am ${i} strange loops.`]);
+  }
+}
+}
+
+function whileLoop(n){
+  while(n > 0){
+    console.log(--n);
+  }
+  return "done";
+}
+
+function doWhileLoop(num){
+  var i = 0;
+  function incrementVariable() {
+  i = i + 1;
+  return i;
+  }
+  
+  do{
+    console.log("I run once regardless.");
+  } while(incrementVariable() < num);
+}
+
+
